46b4f6d330109c32b19eb2bba5dc1e5e91f0d45b
GustavoVasquez <tavovm24@hotmail.com>
2025-09-21 18:26:15 -0300

    ejercicio 2

---
+++ practica1/ejercicio2.asm
@@ -0,0 +1,21 @@
+;EJERCICIO 2
+ORG 1000H
+MSJ DB "ARQUITECTURA DE COMPUTADORAS-"
+    DB "FACULTAD DE INFORMATICA-"
+    DB 55H
+    DB 4EH
+    DB 4CH
+    DB 50H
+FIN DB ?
+ORG 2000H
+MOV BX, OFFSET MSJ
+MOV AL, OFFSET FIN - OFFSET MSJ
+INT 7
+INT 0
+END
+;a) Ejecutar en el simulador ¿qué imprime?
+;ARQUITECTURA DE COMPUTADORAS-FACULTAD DE INFORMATICA-UNLP
+;b) ¿Por qué imprime “UNLP” al final?
+;PORQUE ESOS VALORES HEXADECIMALES REFERENCIAN CADA CARACTER EN FORMATO ASCII
+;c) Con referencia a la interrupción INT 7, ¿qué se almacena en los registros BX y AL?
+;En BX se almacena la dirección desde donde empezara a imprimir y en AL la cantidad de caracteres que se imprimirá por pantalla.
